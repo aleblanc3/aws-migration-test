b16ac5b5541f00b0f7e4b390c18cb29bb253085c
Amber <amber.leblanc@cra-arc.gc.ca>
2025-08-19 15:39:11 -0400

    Add new details component

---
+++ src/app/views/page-assistant/components/page-details.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ca-page-details',
+  imports: [],
+  templateUrl: './page-details.component.html',
+  styles: `
+    :host {
+      display: block;
+    }
+  `
+})
+export class PageDetailsComponent {
+
+}
