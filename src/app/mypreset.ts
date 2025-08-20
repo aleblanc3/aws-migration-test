cd0fc2ef0a41887ba9a1cd9ac89498b83a2566a1
Amber <amber.leblanc@cra-arc.gc.ca>
2025-08-20 17:15:20 -0400

    Adjust colors to improve contrast

--- src/app/mypreset.ts
+++ src/app/mypreset.ts
@@ -1,7 +1,10 @@
 import { definePreset } from '@primeng/themes';
 import Lara from '@primeng/themes/lara';
+import Aura from '@primeng/themes/aura';
+import Nora from '@primeng/themes/nora';
+import Material from '@primeng/themes/material';
 
-const MyPreset = definePreset(Lara, {
+const MyPreset = definePreset(Material, {
     semantic: {
         primary: {
             50: '#f3e9fa',
@@ -17,20 +20,87 @@ const MyPreset = definePreset(Lara, {
             950: '#1d0728',
         },
     },
-        dark: {
-            primary: {
-                50: '#1d0728',
-                100: '#2a0c3c',
-                200: '#391356',
-                300: '#491b71',
-                400: '#5a248c',
-                500: '#6e2ea7',  // base color from project assistant
-                600: '#8f47d3',
-                700: '#a96ddd',
-                800: '#c59ae8',
-                900: '#dfc7f2',
-                950: '#f3e9fa',
-            },
-        }
-    });
+    primitive: {
+        green: { //success 
+            50: '#e6f8f8',
+            100: '#bdeee9',
+            200: '#91e0db',
+            300: '#64d1cd',
+            400: '#3abfbe',
+            500: '#00adae', // base
+            600: '#00939c',
+            700: '#007780',
+            800: '#005d65',
+            900: '#00464b',
+            950: '#002e30',
+        },
+        red: { //danger
+            50: '#ffe5e8',
+            100: '#ffb8c4',
+            200: '#ff8aa0',
+            300: '#ff5c7c',
+            400: '#ff3860',
+            500: '#ff0034', // base
+            600: '#e6002e',
+            700: '#b40026',
+            800: '#86001c',
+            900: '#5a0014',
+            950: '#2f000a',
+        },
+        orange: { //warn
+            50: '#fff2e6',
+            100: '#ffd9b8',
+            200: '#ffc08a',
+            300: '#ffa85c',
+            400: '#ff8f36',
+            500: '#ff7700', // base
+            600: '#e66a00',
+            700: '#b35600',
+            800: '#854200',
+            900: '#592e00',
+            950: '#2f1700',
+        },
+        blue: { //info
+            50: '#e6f0ff',
+            100: '#b8d4ff',
+            200: '#8ab8ff',
+            300: '#5c9cff',
+            400: '#387fff',
+            500: '#0063ff', // base
+            600: '#0057e6',
+            700: '#0046b4',
+            800: '#003386',
+            900: '#001f59',
+            950: '#000f2f',
+        },
+        purple: { //help
+            50: '#f2e6ff',
+            100: '#d9b8ff',
+            200: '#c08aff',
+            300: '#a85cff',
+            400: '#8f36ff',
+            500: '#7700ff', // base
+            600: '#6600e6',
+            700: '#4d00b4',
+            800: '#390086',
+            900: '#260059',
+            950: '#13002f',
+        },
+    },
+    dark: {
+        primary: {
+            50: '#1d0728',
+            100: '#2a0c3c',
+            200: '#391356',
+            300: '#491b71',
+            400: '#5a248c',
+            500: '#6e2ea7',  // base color from project assistant
+            600: '#8f47d3',
+            700: '#a96ddd',
+            800: '#c59ae8',
+            900: '#dfc7f2',
+            950: '#f3e9fa',
+        },
+    }
+});
 export default MyPreset;
