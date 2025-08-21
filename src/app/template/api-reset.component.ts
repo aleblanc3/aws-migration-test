a99a465f960c20012eb609ef7e531018f6e2dbfc
Amber <amber.leblanc@cra-arc.gc.ca>
2025-08-20 22:06:56 -0400

    Update button style

--- src/app/template/api-reset.component.ts
+++ src/app/template/api-reset.component.ts
@@ -7,15 +7,22 @@ import { ButtonModule } from 'primeng/button';
   selector: 'ca-api-reset',
   imports: [TranslateModule, ButtonModule],
   template: `
-    <p-button icon="pi pi-key" [label]="'apiKey.change' | translate" [rounded]="true" [raised]="true" severity="secondary"
-    id="api-key-reset-btn" class="my-2" (click)="this.localStore.removeData('apiKey');" />
+    <p-button icon="pi pi-key" [label]="'apiKey.change' | translate" [rounded]="true" outlined severity="secondary"
+    id="api-key-reset-btn" class="my-2 api-button" styleClass="surface-border" (click)="this.localStore.removeData('apiKey');" />
   `,
   styles: `
   @media (max-width: 768px) {
-  ::ng-deep #api-key-reset-btn .p-button-label {
+  ::ng-deep .api-button .p-button-label {
     display: none;
   }
-}`
+}
+ ::ng-deep .api-button:hover .p-button-icon {
+    color: var(--p-primary-400) !important;
+  }
+
+  ::ng-deep html.dark-mode .api-button:hover .p-button-icon {
+    color: var(--p-primary-200) !important;
+  }`
 })
 export class ApiResetComponent {
   constructor(public localStore: LocalStorageService) { }
